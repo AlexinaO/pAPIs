75de61c6d716975ed38b0c755893befc83e4dbbd
blipn <oblipn@gmail.com>
2020-06-24 11:11:33 +0200

    refactor + new lint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2016,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-plusplus': ['off'],
+    'no-underscore-dangle': ['off'],
+    'consistent-return': ['warn'],
+    'prefer-promise-reject-errors': ['warn'],
+    'semi': ["error", "never"],
+  },
+};
