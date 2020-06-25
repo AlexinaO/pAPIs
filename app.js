75ac4b15db5808e50d69ef0b7e0c091c5d4ac49c
blipn <oblipn@gmail.com>
2020-06-25 11:25:53 +0200

    update logger + swagger

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
-const logger = require('morgan')
 require('dotenv').config()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const logger = require('debug')('web')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger.json')
 const indexRouter = require('./routes/index')
 const apiRouter = require('./routes/api')
 const apiResponse = require('./helpers/apiResponse')
@@ -15,13 +17,13 @@ const { MONGODB_URL } = process.env
 mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
   // don't show the log when it is test
   if (process.env.NODE_ENV !== 'test') {
-    console.log('Connected to %s', MONGODB_URL)
-    console.log('App is running ... \n')
-    console.log('Press CTRL + C to stop the process. \n')
+    logger('Connected to %s', MONGODB_URL)
+    logger('App is running ... \n')
+    logger('Press CTRL + C to stop the process. \n')
   }
 })
   .catch((err) => {
-    console.error('App starting error:', err.message)
+    logger('App starting error:', err.message)
     process.exit(1)
   })
 // eslint-disable-next-line no-unused-vars
@@ -29,14 +31,9 @@ const db = mongoose.connection
 
 const app = express()
 
-// display swagger in test env
-if (process.env.NODE_ENV === 'test') {
-  const swaggerUi = require('swagger-ui-express')
-  const swaggerDocument = require('./swagger.json')
+// display swagger
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
-  app.use(logger('dev'))
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-}
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
