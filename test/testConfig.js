1618c10e301b16865531afcf65e0ad7710a60668
blipn <oblipn@gmail.com>
2020-06-25 19:38:54 +0200

    [Pipeline] no .env required for running tests

--- test/testConfig.js
+++ test/testConfig.js
@@ -1,7 +1,11 @@
-require('dotenv').config()
-
 // During the automated test the env variable, We will set it to "test"
 process.env.NODE_ENV = 'test'
+process.env.MONGODB_URL = 'mongodb://mongo:27017/rest-api-nodejs-mongodb-test'
+process.env.JWT_SECRET = 'TESTSECRET'
+process.env.JWT_TIMEOUT_DURATION = '2 hours'
+process.env.EMAIL_SMTP_HOST = 'in-v3.mailjet.com'
+process.env.EMAIL_SMTP_PORT = 587
+process.env.EMAIL_SMTP_SECURE = true
 
 // Require the dev-dependencies
 const faker = require('faker')
